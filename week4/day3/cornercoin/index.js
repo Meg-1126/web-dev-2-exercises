e73e66c3fb04cdb7124350c23488bc874dfa1139
Megumi Akama <kamayatu91@gmail.com>
2022-06-17 15:48:05 -0700

    Add solution day3

--- week4/day3/cornercoin/index.js
+++ week4/day3/cornercoin/index.js
@@ -1,46 +1,60 @@
-let balance = 500.0;
+// let balance = 500.0;
 
-class Withdrawal {
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
-  }
-}
-// Step 1: Deposit class
-class Deposit {
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance += this.amount;
-  }
-}
-// Step 2: Account class
 class Account {
-  constructor(username) {
+  constructor(username, balance) {
     this.username = username;
     this.balance = balance;
   }
+  sum() {
+    
+  }
 }
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  commit() {
+    this.account.balance += this.value;
+  }
+}
+
+class Withdrawal extends Transaction {
+  
+  // commit() {
+  //   this.account.balance -= this.amount;
+  // }
+  get value () {
+    return -this.amount;
+  }
+}
+// Step 1: Deposit class
+class Deposit extends Transaction {
+  // commit() {
+  //   this.account.balance += this.amount;
+  // }
+  get value () {
+    return this.amount;
+  }
+}
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-const myAccount = new Account('snow-patrol');
+const myAccount = new Account('snow-patrol', 500.0);
+console.log(myAccount.balance);
 
-t1 = new Withdrawal(50.25);
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log(myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
 
-t3 = new Deposit(120.0);
+const t3 = new Deposit(120.0, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
