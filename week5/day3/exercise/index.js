2f2409d993dc4a1b3c0609abde7965b698583409
Megumi Akama <kamayatu91@gmail.com>
2022-06-23 13:58:55 -0700

    Update day3 file

--- week5/day3/exercise/index.js
+++ week5/day3/exercise/index.js
@@ -1,3 +1,51 @@
-$(() => {
+$(document).ready(() => {
   // code here
+  let urls = ['https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/comments'
+];
+  // ## Step 1: Load Posts
+  
+    $.ajax({
+      method: 'get',
+      url: urls[0],
+      dataType: 'json',
+    }).done((data) => {
+        $.each(data, (i, post) => {
+          $('#posts').append(`
+            <div class='post${post.id}' >
+             <h2>${post.title}, ${i}</h2>
+             <p>${post.body}</p>
+             <h3>Comment</h3>
+            </div>
+          `);
+        });
+    });
+  
+
+  // ## Step 2: Load Comments
+   $.ajax({
+    method: 'get',
+    url: urls[1],
+    dataType: 'json',
+   }).done((data) => {
+    
+     console.log('get comments', data);
+     $.each(data, (i, comment) => {
+      let counter = 1;
+      if (comment.postId === 1){
+        $('.post').append(`
+        <div class='comment'>
+          <h2>Comment</h2>
+          <p>${comment.body}</p>
+        
+        </div>
+        `);
+      }
+     })
+   });
+
+
+
+
+
 });
